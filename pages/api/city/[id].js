2a82d660246a3e4614a46832eb1ef0b45825a67e
pkellner <peter@peterkellner.net>
2022-04-08 13:12:04 -0700

    added delay to /api/city

--- pages/api/city/[id].js
+++ pages/api/city/[id].js
@@ -3,6 +3,7 @@
 
 import Cors from "cors";
 import initMiddleware from "../../../src/utils/initMiddleware";
+import runDelayMiddleware from "../../../src/utils/runDelayMiddleware";
 
 
 // Initialize the cors middleware
@@ -15,30 +16,33 @@ const cors = initMiddleware(
 );
 
 export default async function cityDataHandler(req, res) {
-  const cityData = [
-    {
-      id: 6,
-      city: "Chicago",
-      state: "Illinois",
-    },
-    {
-      id: 5,
-      city: "Los Angeles",
-      state: "California",
-    },
-    {
-      id: 4,
-      city: "New York",
-      state: "New York",
-    },
-  ];
-  await cors(req, res)
-  let filtered = {};
-  await new Promise((r) => setTimeout(r, 2000));
-  filtered = cityData.filter((city) => city.id == req.query.id);
-  if (filtered.length > 0) {
-    res.status(200).json(filtered[0]);
-  } else {
-    res.status(404).json({ message: `city with id: ${(req.query.id)} not found.` });
+  await runDelayMiddleware(req, res,2000,2000);
+  {
+    const cityData = [
+      {
+        id: 6,
+        city: "Chicago",
+        state: "Illinois",
+      },
+      {
+        id: 5,
+        city: "Los Angeles",
+        state: "California",
+      },
+      {
+        id: 4,
+        city: "New York",
+        state: "New York",
+      },
+    ];
+    await cors(req, res)
+    let filtered = {};
+    await new Promise((r) => setTimeout(r, 2000));
+    filtered = cityData.filter((city) => city.id == req.query.id);
+    if (filtered.length > 0) {
+      res.status(200).json(filtered[0]);
+    } else {
+      res.status(404).json({message: `city with id: ${(req.query.id)} not found.`});
+    }
   }
 }
--- pages/api/city/index.js
+++ pages/api/city/index.js
@@ -1,5 +1,6 @@
 import Cors from "cors";
 import initMiddleware from "../../../src/utils/initMiddleware";
+import runDelayMiddleware from "../../../src/utils/runDelayMiddleware";
 
 
 
@@ -13,24 +14,44 @@ const cors = initMiddleware(
 );
 
 export default async function handler(req, res) {
-  const cityData = [
-    {
-      id: 6,
-      city: "Chicago",
-      state: "Illinois",
-    },
-    {
-      id: 5,
-      city: "Los Angeles",
-      state: "California",
-    },
-    {
-      id: 4,
-      city: "New York",
-      state: "New York",
-    },
-  ];
-  await cors(req, res)
-  await new Promise((r) => setTimeout(r, 2000));
-  res.status(200).json(cityData);
+  await runDelayMiddleware(req, res,2000,2000);
+  {
+    const cityData = [
+      {
+        id: 6,
+        city: "Chicago",
+        state: "Illinois",
+      },
+      {
+        id: 5,
+        city: "Los Angeles",
+        state: "California",
+      },
+      {
+        id: 4,
+        city: "New York",
+        state: "New York",
+      },
+    ];
+    await cors(req, res)
+    await new Promise((r) => setTimeout(r, 2000));
+    res.status(200).json(cityData);
+  }
+  
+ 
 }
+
+
+/*
+await runDelayMiddleware(req, res);
+  {
+    let count = 9999;
+    if (req?.query?.count) {
+      count = parseInt(req.query.count);
+    }
+
+    const resultData = await getCitiesData(count);
+
+    res.status(200).json(JSON.stringify(resultData, null, "\t"));
+  }
+ */
